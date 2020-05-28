edae1f9770a370f0a83b77b71a2ff9744e36fd71
rohit <rohit@rohit-Vostro-1015>
2020-05-28 17:42:49 +0530

    Exception related changes.

--- git-repository-access/src/main/java/com/gitrepositoryaccess/modifiedfilesondate/ReadListOfModifiedFilesOnDate.java
+++ git-repository-access/src/main/java/com/gitrepositoryaccess/modifiedfilesondate/ReadListOfModifiedFilesOnDate.java
@@ -18,22 +18,26 @@ import com.gitrepositoryaccess.util.UtilityFile;
 
 public class ReadListOfModifiedFilesOnDate {
 	private final static Logger logger = Logger.getLogger(ReadListOfModifiedFilesOnDate.class.getName());
-	
-	public static void main(String[] args) throws IOException, ParseException {
+
+	public static void main(String[] args) throws IOException {
 		getFilesOnDate();
 		logger.info("Request GET DONE");
 	}
 
-	private static void getFilesOnDate() throws ParseException, IOException {
-		StringBuffer response = UtilityFile.connectToUrl();
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the Date ");
-
-		String date = scanner.next();
-
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
-		String strDate = dateFormat.format(dateFormat.parse(date));
+	private static void getFilesOnDate() throws IOException {
+		StringBuffer response = null;
+		String strDate = null;
+		try {
+			response = UtilityFile.connectToUrl();
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter the Date ");
+			String date = scanner.next();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+			strDate = dateFormat.format(dateFormat.parse(date));
+		} catch (ParseException e) {
+			throw new org.apache.http.ParseException("Please enter date in yyyy-mm-dd format." + e);
+		}
 		System.out.println("Files modified on date :" + strDate);
 
 		List<String> data = Arrays.stream(response.toString().split("\"commit\":")).skip(1)
@@ -51,29 +55,35 @@ public class ReadListOfModifiedFilesOnDate {
 		}
 	}
 
-	private static String getModifiedFilesName(String collect) throws IOException {
-		Properties property = UtilityFile.getValuesFromProperty();
-		URL obj = new URL(collect);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", property.getProperty("USER_AGENT"));
-		int responseCode = con.getResponseCode();
-		logger.info("GET Response Code :: " + responseCode);
+	private static String getModifiedFilesName(String collect) {
+		Properties property;
 		String fileName = null;
-		if (responseCode == HttpURLConnection.HTTP_OK) {
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		try {
+			property = UtilityFile.getValuesFromProperty();
+			URL obj = new URL(collect);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", property.getProperty("USER_AGENT"));
+			int responseCode = con.getResponseCode();
+			logger.info("GET Response Code :: " + responseCode);
 
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-				fileName = Arrays.stream(response.toString().split("\"filename\":")).skip(1).map(l -> l.split(",")[0])
-						.collect(Collectors.joining("\n"));
-				System.out.println(fileName);
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+					fileName = Arrays.stream(response.toString().split("\"filename\":")).skip(1)
+							.map(l -> l.split(",")[0]).collect(Collectors.joining("\n"));
+					System.out.println(fileName);
+				}
+				in.close();
+			} else {
+				logger.info("GET request not worked");
 			}
-			in.close();
-		} else {
-			logger.info("GET request not worked");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return fileName;
 	}
--- git-repository-access/src/main/java/com/gitrepositoryaccess/util/UtilityFile.java
+++ git-repository-access/src/main/java/com/gitrepositoryaccess/util/UtilityFile.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.ParseException;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -21,27 +20,31 @@ public class UtilityFile {
 		return properties;
 	}
 
-	public static StringBuffer connectToUrl() throws IOException, ParseException {
-		// ReadPropertyFromFile readProperty = new ReadPropertyFromFile();
-		Properties property = UtilityFile.getValuesFromProperty();
-		URL obj = new URL(property.getProperty("url"));
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-		con.setRequestMethod("GET");
-		con.setRequestProperty("User-Agent", property.getProperty("USER_AGENT"));
-		int responseCode = con.getResponseCode();
-		logger.info("GET Response Code :: " + responseCode);
-		if (responseCode == HttpURLConnection.HTTP_OK) {
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-
-				return response.append(inputLine);
-			}
-			in.close();
-		} else {
+	public static StringBuffer connectToUrl() {
+		Properties property;
+		try {
+			property = UtilityFile.getValuesFromProperty();
+			URL obj = new URL(property.getProperty("url"));
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", property.getProperty("USER_AGENT"));
+			int responseCode = con.getResponseCode();
 			logger.info("GET Response Code :: " + responseCode);
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+				while ((inputLine = in.readLine()) != null) {
+
+					return response.append(inputLine);
+				}
+				in.close();
+			} else {
+				logger.info("GET Response Code :: " + responseCode);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return null;
-	}
+	}	
 }
